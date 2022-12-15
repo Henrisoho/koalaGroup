dbc8ec3a2127ed40518c2bbe9650c10adefe1128
henrisoho <bakingwithmymother@gmail.com>
2022-12-15 14:37:03 -0600

    Finito

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,13 +2,12 @@ console.log( 'js' );
 
 $( document ).ready( function(){
   console.log( 'JQ' );
-  // Establish Click Listeners
   setupClickListeners()
-  // load existing koalas on page load
   getKoalas();
-
+  $('body').on('click', '.transferButton', transferKoala);
 }); // end doc ready
 
+//CREATE KOALA
 function setupClickListeners() {
   $( '#addButton' ).on( 'click', function(){
     console.log( 'in addButton on click' );
@@ -16,25 +15,87 @@ function setupClickListeners() {
     // NOT WORKING YET :(
     // using a test object
     let koalaToSend = {
-      name: 'testName',
-      age: 'testName',
-      gender: 'testName',
-      readyForTransfer: 'testName',
-      notes: 'testName',
+      name: $('#nameIn').val(),
+      age: $('#ageIn').val(),
+      gender: $('#genderIn').val(),
+      ready_to_transfer: $('#readyForTransferIn').val(),
+      notes: $('#notesIn').val(),
     };
     // call saveKoala with the new obejct
     saveKoala( koalaToSend );
   }); 
 }
 
+//KOALA RENDER
 function getKoalas(){
   console.log( 'in getKoalas' );
-  // ajax call to server to get koalas
-  
+  $.ajax({
+    type: 'GET',
+    url: '/koalas'
+  }).then(function(response) {
+    $('#viewKoalas').empty();
+    for (let koala of response) {
+    if(koala.ready_to_transfer == 'Y'){
+      $('#viewKoalas').append(`
+        <tr>
+          <td>${koala.name}</td>
+          <td>${koala.gender}</td>
+          <td>${koala.age}</td>
+          <td>${koala.ready_to_transfer}</td>
+          <td>${koala.notes}</td>
+        </tr>
+    `);
+    } else{
+       $('#viewKoalas').append(`
+        <tr>
+          <td>${koala.name}</td>
+          <td>${koala.gender}</td>
+          <td>${koala.age}</td>
+          <td>${koala.ready_to_transfer}</td>
+          <td>${koala.notes}</td>
+          <td><button data-id=${koala.id} class="transferButton">Ready To Transfer</button><td>
+        </tr>
+    `);
+    }
+    console.log(koala.id)
+    }
+  }).catch(function(error){
+    console.log('error in GET', error);
+  });
 } // end getKoalas
 
+
+//KOALA POST ROUTE
 function saveKoala( newKoala ){
   console.log( 'in saveKoala', newKoala );
   // ajax call to server to get koalas
- 
+  $.ajax({
+    type: 'POST',
+    url: '/koalas',
+    data: newKoala,
+    }).then(function(response) {
+      console.log('Response from server.', response);
+      getKoalas();
+    }).catch(function(error) {
+      console.log('Error in POST', error)
+      alert('Unable to add koala at this time. Please try again later.');
+    });
+}
+
+//UPDATE TRANSFER STATUS
+function transferKoala() {
+  let idToUpdate = $(this).data().id;
+  
+  $.ajax({
+    method: 'PUT',
+    url: `/koalas/${idToUpdate}`,
+    data: {
+      ready_to_transfer: 'Y'
+    }
+  }).then((response) => {
+    console.log(idToUpdate)
+    getKoalas();
+  }).catch((error) => {
+    console.log(error);
+  })
 }
--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -1,17 +1,70 @@
 const express = require('express');
 const koalaRouter = express.Router();
+const pool = require('../modules/pool.js');
 
 // DB CONNECTION
 
 
 // GET
+koalaRouter.get('/', (req, res) => {
+  let sqlQuery = `
+    SELECT * FROM "koalas" 
+      ORDER BY "id";
+  `;
+  pool.query(sqlQuery)
+    .then((dbRes) => {
+      res.send(dbRes.rows);
+    })
+    .catch((dbErr) => {
+      console.log('error', dbErr);
+      res.sendStatus(500);
+    });
+});
 
 
 // POST
+koalaRouter.post('/',  (req, res) => {
+  let newKoala = req.body;
+  console.log(newKoala);
 
+  let sqlQuery = `
+    INSERT INTO "koalas" 
+    ("name", "gender", "age", "ready_to_transfer", "notes")
+    VALUES ($1, $2, $3, $4, $5);
+  `;
+  let sqlValues = [newKoala.name, newKoala.gender, newKoala.age, newKoala.ready_to_transfer, newKoala.gender];
+  pool.query(sqlQuery, sqlValues)
+    .then((dbRes) => {
+      res.sendStatus(201);
+    })
+    .catch((dbErr) => {
+      console.log(dbErr);
+      res.sendStatus(500);
+    });
+});
 
 // PUT
+koalaRouter.put('/:id', (req, res) => {
+  console.log('req.params:', req.params);
+  console.log('req.body:', req.body);
+  let idToUpdate = req.params.id;
+  let transferStatus = req.body.ready_to_transfer;
 
+  let sqlQuery = `
+    UPDATE "koalas"
+	    SET "ready_to_transfer"=$1
+	    WHERE "id"=$2;
+  ` 
+  let sqlValues = [transferStatus, idToUpdate];
+  pool.query(sqlQuery, sqlValues)
+    .then((dbRes) => {
+      res.sendStatus(200);
+    })
+    .catch((dbErr) => {
+      console.log(dbErr);
+      res.sendStatus(500);
+    })
+})
 
 // DELETE
 
